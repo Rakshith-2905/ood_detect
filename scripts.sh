3272103eb38f0bd43e8acf38dd64ec25fd30e183
Rakshith <rakshith-2905@gmail.com>
2023-11-02 20:55:34 -0700

    Fix: chenged proj dim to 1024

--- scripts.sh
+++ scripts.sh
@@ -12,12 +12,12 @@ python cam_masking.py --model_path 'logs/classifier/resnet_resnet18/best_model_w
 
 python train_projection.py --dataset domainnet --domain real --image_size 224 --batch_size 64 --seed 42  \
                             --resnet_model resnet50 --checkpoint_path 'logs/classifier/resnet50_domainnet_real/best_checkpoint.pth' \
-                            --resnet_dim 2048 --projection_dim 512 --teacher_temp 0.5 --student_temp 0.1 \
+                            --resnet_dim 2048 --projection_dim 1024 --teacher_temp 0.5 --student_temp 0.1 \
                             --prompt_embeddings_pth "prompts/CLIP_RN50_text_embeddings.pth" --use_default_prompt True \
                             --similarity_mode "DN"
 
 python test_projection.py --dataset domainnet --domain real --image_size 224 --batch_size 64 --seed 42  \
                             --resnet_model resnet50 --checkpoint_path 'logs/classifier/resnet50_domainnet_real/best_checkpoint.pth'  \
                             --projector_checkpoint_path 'logs/classifier/resnet50_domainnet_real/projection_default_prompt_gt_sim0_distill1_DN_mapping1/projector_weights.pth' \
-                            --resnet_dim 2048 --projection_dim 512  \
+                            --resnet_dim 2048 --projection_dim 1024  \
                             --prompt_embeddings_pth "prompts/CLIP_RN50_text_embeddings.pth" --similarity_mode "DN"
