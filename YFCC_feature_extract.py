25586dd3028e5e1f25bb3ed27c442ea4465e80f5
kowshikthopalli <kthopall@asu.edu>
2023-11-12 14:48:11 -0800

    fix: feats fabric

--- YFCC_feature_extract.py
+++ YFCC_feature_extract.py
@@ -28,6 +28,10 @@ import random
 from models.ViT_models import SAMBackbone, MAEBackbone, DINOBackbone
 from models.resnet import CustomFeatureModel, CustomSegmentationModel
 from models.projector import ProjectionHead
+import lightning as L
+from lightning.fabric import Fabric, seed_everything
+from lightning.fabric.loggers import TensorBoardLogger, CSVLogger
+
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -85,7 +89,7 @@ def progbar_wrapper(iterable, total, **kwargs):
     return iterable
 
 @torch.no_grad()
-def process_data_loader(rank, world_size, args, feature_extractor, device, transform, save_path, clip_model_name, feature_extractor_name):
+def process_data_loader(args, feature_extractor, device, transform, save_path, clip_model_name, feature_extractor_name):
     if fabric.is_global_zero:
         os.makedirs(save_path, exist_ok=True)
     
@@ -105,8 +109,8 @@ def process_data_loader(rank, world_size, args, feature_extractor, device, trans
         'filenames': [],
         'captions': []
     }
-    processed_images = tensor(0)
-
+    processed_images = torch.tensor(0)
+    fabric.print(processed_images)
     for images_batch, captions_batch, image_names_batch in tqdm(data_loader, desc="Processing batches", unit="batch"):
         images_batch = images_batch.to(device)
         captions_batch = [caption for caption in captions_batch]
@@ -115,6 +119,8 @@ def process_data_loader(rank, world_size, args, feature_extractor, device, trans
         image_features = feature_extractor(images_batch)
         text_tokens = fabric.to_device(clip.tokenize(captions_batch, truncate=True))
         text_features = clip_model.encode_text(text_tokens)
+        fabric.print(f'text_features.shape: {text_features.shape}')
+        fabric.print(f'image_features.shape: {image_features.shape}') 
 
         # Gather features and filenames
         gathered_image_features = fabric.all_gather(image_features).view(-1, image_features.shape[-1])
@@ -123,12 +129,14 @@ def process_data_loader(rank, world_size, args, feature_extractor, device, trans
         gathered_captions = fabric.all_gather(captions_batch)
 
         processed_images += gathered_image_features.shape[0]
-
+        fabric.print(f'gathered_image_features.shape: {gathered_image_features.shape}, gathered_text_features.shape: {gathered_text_features.shape} ')
+        fabric.print(f'processed_images: {processed_images}')
+      
         accumulated_data['image_features'].append(gathered_image_features)
         accumulated_data['text_features'].append(gathered_text_features)
         accumulated_data['filenames'].extend(gathered_filenames)
         accumulated_data['captions'].extend(gathered_captions)
-
+        
         # Save and reset after processing X number of images
         if processed_images >= args.images_per_chunk:
             save_chunk_data(accumulated_data, save_path, feature_extractor_name, clip_model_name)
@@ -152,7 +160,8 @@ def save_chunk_data(accumulated_data, save_path, feature_extractor_name, clip_mo
     # Convert lists of tensors to a single tensor
     image_features_tensor = torch.cat(accumulated_data['image_features'], dim=0).detach().cpu()
     text_features_tensor = torch.cat(accumulated_data['text_features'], dim=0).detach().cpu()
-
+    print(f'image_features_tensor.shape: {image_features_tensor.shape}, text_features_tensor.shape: {text_features_tensor.shape}')
+    
     # Prepare data for saving
     save_data = {
         'image_features': image_features_tensor,
@@ -166,6 +175,8 @@ def save_chunk_data(accumulated_data, save_path, feature_extractor_name, clip_mo
 
     # Save the combined data
     torch.save(save_data, save_filename)
+    
+    assert False
 
 
 def get_transform(feature_extractor_name):
@@ -249,9 +260,21 @@ if __name__ == "__main__":
     parser.add_argument('--data_path', required=True, help='Path to the data.')
     parser.add_argument('--batch_size', type=int, default=1024, help='Batch size for the feature extractor.')
     parser.add_argument('--feature_extractor_checkpoint_path', type=str)
-    parser.add_argument('--num_batches_per_chunk', type=int, default=100, help='How many batches make a chunk.')
+    parser.add_argument('--images_per_chunk', type=int, default=100, help='How many batches make a chunk.')
     parser.add_argument('--start_index', type=int, default=0, help='The starting line index in the JSON file.')
     parser.add_argument('--end_index', type=int, required=True, help='The ending line index in the JSON file.')
-
+    # add num_nodes
+    parser.add_argument('--num_nodes', type=int, default=2, help='Number of nodes for distributed training.')
+    # add num_gpus
+    parser.add_argument('--num_gpus', type=int, default=4, help='Number of gpus per node for distributed training.')
     args = parser.parse_args()
+    
+    fabric = L.Fabric(accelerator="cuda",num_nodes=args.num_nodes, devices=args.num_gpus, strategy="auto")
+   
+    fabric.launch()
+    
+
+    # The total number of processes running across all devices and nodes
+    fabric.print(f"World size: {fabric.world_size}")  # 2 * 3 = 6
+
     main(args)
