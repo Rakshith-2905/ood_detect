4761c1bd83ae8932b82e642e7ad79fcd660fc852
Rakshith <rakshith-2905@gmail.com>
2023-11-11 20:05:42 -0700

    Feat: Inprogress deeplabv3_resnet50 support

--- models/resnet.py
+++ models/resnet.py
@@ -1,8 +1,12 @@
 import torch
 import torch.nn as nn
 from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152
+from torchvision.models.segmentation import deeplabv3_resnet50, deeplabv3_resnet101, DeepLabV3_ResNet50_Weights, DeepLabV3_ResNet101_Weights
+from torchvision.models.segmentation import fcn_resnet50, fcn_resnet101, FCN_ResNet50_Weights, FCN_ResNet101_Weights
 from torchvision import transforms
+from torchvision.models.feature_extraction import get_graph_node_names, create_feature_extractor
 
+from PIL import Image
 import timm
 
 class CustomFeatureModel(nn.Module):
@@ -29,6 +33,78 @@ class CustomFeatureModel(nn.Module):
 
     def forward(self, x):
             return self.model(x)
+
+class CustomSegmentationModel(nn.Module):
+    def __init__(self, model_name, use_pretrained=False):
+        super(CustomSegmentationModel, self).__init__()
+
+        supported_models = ['deeplabv3_resnet50', 'deeplabv3_resnet101', 'fcn_resnet50', 'fcn_resnet101']
+        if model_name not in supported_models:
+            raise ValueError(f"Invalid model_name. Expected one of {supported_models}, but got {model_name}")
+
+        if model_name == 'deeplabv3_resnet50':
+            weights = DeepLabV3_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1
+        elif model_name == 'deeplabv3_resnet101':
+            weights = DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1
+        elif model_name == 'fcn_resnet50':
+            weights = FCN_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1
+        elif model_name == 'fcn_resnet101':
+            weights = FCN_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1
+
+        self.model = torch.hub.load('pytorch/vision:v0.6.0', model_name, weights=weights)
+
+        self.transform = transforms.Compose([
+                        transforms.RandomResizedCrop(224),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225])                
+                    ])
+        self.test_transform = transforms.Compose([
+                        transforms.Resize(256, interpolation=3),
+                        transforms.CenterCrop(224),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225])                
+                    ])
+
+        self.transform = transforms.Compose([
+                            transforms.Resize(520),
+                            transforms.CenterCrop(520),
+                            transforms.ToTensor(),
+                            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225])                
+                        ])
+                        
+        self.feature_model = self.model.backbone
+        # Add a max pooling layer with stride 16 to reduce the dimensionality of the features
+        self.pool = nn.MaxPool2d(kernel_size=16, stride=16)
+
+    
+    def preprocess_pil(self, images):
+        # check if images is a list, then preprocess each image
+        if isinstance(images, list):
+            images_torch = []
+            for image in images:
+                image_tensor = self.transform(image)
+                images_torch.append(image_tensor)
+            images_torch = torch.stack(images_torch).to(device) 
+        else:
+            images_torch = self.transform(images).unsqueeze(0).to(device)
+        
+        return images_torch
+
+    @torch.no_grad()
+    def forward(self, images):
+        if not isinstance(images, torch.Tensor):
+            images = self.preprocess_pil(images)
+        else:
+            if len(images.shape) == 3:
+                images = images.unsqueeze(0)
+        features = self.feature_model(images)['out']
+        features = self.pool(features)
+        
+        return features
 
 class CustomResNet(nn.Module):
     def __init__(self, model_name, num_classes, use_pretrained=False):
@@ -79,8 +155,16 @@ class CustomResNet(nn.Module):
 
 if __name__ == "__main__":
 
-    model = CustomFeatureModel(model_name='resnet101', use_pretrained=True)
-    features = model(torch.zeros(1, 3, 224, 224))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # model = CustomFeatureModel(model_name='resnet101', use_pretrained=True)
+    # features = model(torch.zeros(1, 3, 224, 224))
+    # print(features.shape)
+    # print(model.feature_dim)
+
+    model = CustomSegmentationModel(model_name='deeplabv3_resnet101', use_pretrained=True).to(device)
+    pil_image = Image.open("./data/domainnet_v1.0/real/toothpaste/real_318_000284.jpg")
+    pil_images = [pil_image]
+    torch_tensor = torch.zeros(3, 224, 224).to(device)
+    features = model(pil_images)
     print(features.shape)
-    print(model.feature_dim)
 
